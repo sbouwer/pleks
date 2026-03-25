e1467a5c14614d6f36a2fbdfaf87f51ca3085639
Stéan Bouwer <stean@yoros.co.za>
2026-03-25 13:06:42 +0200

    Suppress hydration warning on body (Grammarly extension injects attributes)

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${plusJakartaSans.variable} ${dmSans.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
